d9c357fa8a487697cff7283286423f1bea167dc9
matheus <matheusferreirasantanaqwe@gmail.com>
2022-04-07 21:29:14 -0300

    versoa basica

---
+++ app/client/scripts/alternarvideos.js
@@ -0,0 +1,11 @@
+function ramdomNumber(x, y) {
+    return Math.floor(Math.random() * (y - x + 1)) + x;
+}
+
+function alterandoVideos() {
+    const sources = ["./moves/maosescrevendocodigo.mp4", "./moves/programando.mp4", "./moves/Typing_dark_03_Videvo_preview.mp4"]
+    var video = document.querySelector("#video");
+    video.src = sources[ramdomNumber(0, 2)];
+}
+
+window.setInterval(alterandoVideos, 40000);
